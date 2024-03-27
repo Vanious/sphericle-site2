e4a63149eb1df8f1d140d11c494846c8a1204a5a
vanious <163776893+Vanious@users.noreply.github.com>
2024-03-26 20:19:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from pyscript import document
 
 
 def runCalculate(event):
-    input_text = float(document.querySelector("#calc").getAttribute("value"))
-    
+   input_value = document.querySelector("#calc").getAttribute("value")
+if input_value is not None:
+    input_text = float(input_value)
+else:
     if input_text.value > 1:
         run = input_text.value / 100
     elif input_text.value < 0:
